eeaf9402742418d933004015956a93104e7b84cb
Yandutov Evgeny <86425016+YandutovEvgeny@users.noreply.github.com>
2026-03-18 14:48:23 +0300

    Implemented sku rules units tests and some refactoring (#8)
    Co-authored-by: Евгений Яндутов <yandutka159@gmail.com>

---
+++ tests/PlanningService.Tests.Unit/Fixtures/RulesFixture.cs
@@ -0,0 +1,33 @@
+﻿using PlanningService.Application.Interfaces;
+using System.Reflection;
+
+namespace PlanningService.Tests.Unit.Fixtures;
+
+public class RulesFixture
+{
+    public IReadOnlyDictionary<Type, IFormulaRule> Rules { get; }
+
+    public RulesFixture()
+    {
+        var assembly = Assembly.GetAssembly(typeof(IFormulaRule));
+
+        var ruleTypes = assembly!.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && typeof(IFormulaRule).IsAssignableFrom(t))
+            .ToList();
+
+        var rulesDictionary = new Dictionary<Type, IFormulaRule>();
+
+        foreach (var type in ruleTypes)
+        {
+            if (Activator.CreateInstance(type) is IFormulaRule rule)
+            {
+                rulesDictionary[type] = rule;
+                continue;
+            }
+
+            throw new InvalidOperationException($"Can not create formula rule of type: {type.Name}");
+        }
+
+        Rules = rulesDictionary;
+    }
+}
